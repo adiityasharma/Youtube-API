dbb70aef772d5683172d5a4ca317d2480514934b
Aditya Sharma <adityasharma626367@gmail.com>
2025-07-13 01:27:02 +0530

    feat: add JWT authentication to login route and update user model integration

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,10 @@
 import express, { Router } from "express";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 import cloudinary from "../config/cloudinary.js";
 import User from "../models/user.model.js";
 
@@ -31,7 +36,55 @@ router.post("/signup", async (req, res) => {
 
   } catch (error) {
     console.log(error)
-    throw new Error("Error while uploading image: ", error)
+    res.status(500).json({ error: "something went wrong", message: error.message });
+  }
+})
+
+router.post("/login", async (req, res) => {
+  // console.log(req.body)
+
+  const { email, password } = req.body;
+
+  if (!email && !password) {
+    console.log("Username or password required", error);
+    throw new Error(error);
+  }
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({ error: 'Invalid Credincials' });
+    }
+
+    const checkedPassword = await bcrypt.compare(password, user.password);
+
+    if (!checkedPassword) {
+      return res.status(401).json({ error: "Invalid Credincials" })
+    }
+
+    const token = jwt.sign(
+      {
+        userId: user._id,
+        email: user.email
+      },
+      process.env.JWT_TOKEN,
+      { expiresIn: "3d" }
+    );
+
+    res.status(200).json({
+      message: "Login Successful",
+      token,
+      user: {
+        id: user._id,
+        email: user.email,
+        username: user.username
+      }
+    })
+
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ error: "something went wrong", message: error.message });
   }
 })
 
